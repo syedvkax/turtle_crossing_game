c24703fc877f88125b1fe4af630ad41a9ea259a9
SyedVkax <syed.vkax@gmail.com>
2022-02-25 01:50:00 +0500

    Initial Commit

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 30:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.is_at_finish_line():
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,4 @@
 from turtle import Turtle
-from typing_extensions import Self
 
 FONT = ("Courier", 24, "normal")
 
@@ -14,6 +13,16 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(-280, 250)
         self.write(f"Level: {self.level}", align="left", font=FONT)
-        
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)    
+
     def increase_level(self):
         self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="left", font=FONT)
